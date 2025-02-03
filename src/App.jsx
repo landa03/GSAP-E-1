cb04d89a09ff089498aab09d5b5e3c73d6f65420
landita03 <omarlandita03@gmail.com>
2025-02-03 15:50:07 -0600

    ya esta mapeado, nomas falta acomodarlo y hacer que se mueva correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -14,51 +14,57 @@ function App() {
 
   // let [count, setCount] = useState(0)
   
-  const [oldPromoState, setOldPromoState] = useState(promoTitle.length - 1);
-  const [newPromoState, setNewPromoState] = useState(0);
+  // const [oldPromoState, setOldPromoState] = useState(promoTitle.length - 1);
+  // const [newPromoState, setNewPromoState] = useState(0);
 
   let promoContainerRef = useRef();
 
   // var timeLine1 = new Timeline({onComplete: selectNextPromo});
-  var timeLine1 = new Timeline({onStart: selectNextPromo, onComplete: restartAnimation});
+  // var timeLine1 = new Timeline({onStart: selectNextPromo, onComplete: restartAnimation});
+  var timeLine1 = new Timeline();
   useGSAP(() => {
     timeLine1.to(promoContainerRef.current,{
         x: 450, 
         duration: 3,
         delay: 3,
-        repeat: 0,
-        repeatDelay: 0,
+        repeat: -1,
+        // repeat: promoTitle.length,
+        repeatDelay: 3,
       }
     );
+    // timeLine1.to(promoContainerRef.current,{
+    //   x: -(450 * promoTitle.length),
+    //   duration: 0,
+    // });
   });
 
-  var oldPromoIndex = promoTitle.length - 1;
-  var newPromoIndex = 0;
+  // var oldPromoIndex = promoTitle.length - 1;
+  // var newPromoIndex = 0;
 
-  function restartAnimation() {
+  // function restartAnimation() {
     // setNewPromoState(newPromoIndex);
     // setOldPromoState(oldPromoIndex);
-    timeLine1.progress(0);
-    timeLine1.play();
-    console.log(promoMessage[oldPromoState], promoMessage[newPromoState]);
+    // timeLine1.progress(0);
+    // timeLine1.play();
+    // console.log(promoMessage[oldPromoState], promoMessage[newPromoState]);
     
-  }
+  // }
   
-  function selectNextPromo(){
+  // function selectNextPromo(){
     // setPromoState(promoState + 1);
     // console.log("next promo" ,oldPromoIndex ,newPromoIndex);
-    console.log(promoTitle[oldPromoIndex], promoTitle[newPromoIndex]);
-    if (newPromoIndex >= (promoTitle.length - 1)) {
-      newPromoIndex = 0;
-      oldPromoIndex = promoTitle.length - 1;
-    }else {
-      newPromoIndex ++;
-      setNewPromoState(newPromoIndex);
-      oldPromoIndex = newPromoIndex - 1;
-    }
+    // console.log(promoTitle[oldPromoIndex], promoTitle[newPromoIndex]);
+    // if (newPromoIndex >= (promoTitle.length - 1)) {
+      // newPromoIndex = 0;
+      // oldPromoIndex = promoTitle.length - 1;
+    // }else {
+      // newPromoIndex ++;
+      // setNewPromoState(newPromoIndex);
+      // oldPromoIndex = newPromoIndex - 1;
+    // }
     // setNewPromoState(newPromoIndex);
     // setOldPromoState(oldPromoIndex);
-  }
+  // }
 
   return (
     // <div className='App'>
@@ -70,23 +76,27 @@ function App() {
 
       <div className='container' ref={promoContainerRef}>
         
-        <div className='newPromo'>
-          <div className='title'>
-            {promoTitle[newPromoIndex]}
-            {/* Tailored software, perfect fit. */}
-            {/* {promoMessage['promo-1-title']} */}
+        {promoTitle.map((promoTitle, index) =>(
+
+          <div className='newPromo'>
+            <div className='title'>
+              {promoTitle}
+              {/* Tailored software, perfect fit. */}
+              {/* {promoMessage['promo-1-title']} */}
+            </div>
+            <div className='message'>
+              {promoMessage[index]}
+            </div>
           </div>
-          <div className='message'>
-            {promoMessage[newPromoIndex]}
-          </div>
-        </div>
         
+        ))}
+
         <div className='oldPromo'>
           <div className='title'>
-            {promoTitle[oldPromoIndex]}
+            {promoTitle[0]}
           </div>
           <div className='message'>
-            {promoMessage[oldPromoIndex]}
+            {promoMessage[0]}
           </div>
         </div>
 
